d0de5a41524a03c9d3ed83add75b9dacec141ce6
Bladen Ward <bdw21@student.uwf.edu>
2024-03-08 17:46:43 -0600

    chore: moved keys out of source

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import BackgroundMap from './BackgroundMap.jsx'
 import './App.css'
 
-function App() {
+export default function App() {
     const location = { lat: 30.5446, lng: -87.2120 };
+    const mapId = import.meta.env.VITE_GOOGLE_MAPS_MAP_ID
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+
     return (
         <div style={{ position: 'absolute', top: '0', left: '0', width: '100vw', height: '100vh' }}>
-            <BackgroundMap location={location}/>
+            <BackgroundMap location={location} map_id={mapId} api_key={apiKey}/>
         </div>
     );
 }
-
-export default App
--- src/BackgroundMap.jsx
+++ src/BackgroundMap.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow, useMap } from '@vis.gl/react-google-maps'
 
-export default function BackgroundMap({ location }) {
+export default function BackgroundMap({ location, api_key, map_id }) {
 
     const _onClick = (e) => {
         console.log(JSON.stringify(e.detail.latLng));
@@ -9,19 +9,17 @@ export default function BackgroundMap({ location }) {
 
     return (
         // TODO: move keys and ids to env
-        <APIProvider apiKey="AIzaSyBH2Hs12RUpu_z2mTTex0FBnxRi9diSRHM">
-            <div style={{ height: '100vh', width: '100wh' }}>
-                <Map
-                    defaultZoom={12}
-                    defaultCenter={location}
-                    mapId="83e6a4c4d2e69d8d"
-                    gestureHandling={'greedy'}
-                    disableDefaultUI={true}
-                    onClick={_onClick}
-                >
-                    <AdvancedMarker position={location}/>
-                </Map>
-            </div>
+        <APIProvider apiKey={api_key}>
+            <Map
+                defaultZoom={12}
+                defaultCenter={location}
+                mapId={map_id}
+                gestureHandling={'greedy'}
+                disableDefaultUI={true}
+                onClick={_onClick}
+            >
+                <AdvancedMarker position={location}/>
+            </Map>
         </APIProvider>
     );
 }
