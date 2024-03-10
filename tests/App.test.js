fa2ab27e4970cb3ce0afef6d3fac22b5766199b9
Bladen Ward <Bladenward1213@gmail.com>
2024-03-10 14:38:26 -0500

    build: modify gitignore

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from '../src/App.jsx';
+import BackgroundMap from '../src/components/BackgroundMap/BackgroundMap.jsx'
+
+const location = { lat: 30.5446, lng: -87.2120 };
 
 test('Sample test', () => {
-    render(<App />);
+    render(<BackgroundMap location={location} />);
     // const linkElement = screen.getByText("Temp Fix Travel Planner");
     // expect(linkElement).toBeInTheDocument();
     expect(1 + 2).toBe(3)
