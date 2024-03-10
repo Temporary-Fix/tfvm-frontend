2c1dc76f8535c84bdc6481c44b5f0e255ca49a47
Chase Lamkin <chasemlamkin@gmail.com>
2024-03-09 19:34:31 -0600

    Completed login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import App from './App'
+import './styles/index.css'
+import { themeOptions } from './styles/theme'
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+let theme = createTheme(themeOptions);
+theme = responsiveFontSizes(theme);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <App />
+        </ThemeProvider>
+    </BrowserRouter>,
 )
